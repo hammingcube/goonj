f800d6d41a5459a8166ecb7dd60efac8a9cab89a
maddyonline <madhavjha@gmail.com>
2015-03-23 22:12:22 -0700

    minor

--- cui_server/server.go
+++ cui_server/server.go
@@ -7,6 +7,6 @@ import (
 
 func main() {
 	// Simple static webserver:
-	http.Handle("/cui/", http.StripPrefix("/cui/", http.FileServer(http.Dir("cui/"))))
+	http.Handle("/cui/", http.StripPrefix("/cui/", http.FileServer(http.Dir("../static_cui/cui/"))))
 	log.Fatal(http.ListenAndServe(":8082", nil))
 }
