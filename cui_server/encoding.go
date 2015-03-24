a713184cdd25fede7d3e41e002f79e8f7b3bd196
maddyonline <madhavjha@gmail.com>
2015-03-24 03:54:27 -0700

    minor

---
+++ cui_server/encoding.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"encoding/json"
+	"encoding/xml"
+)
+
+func getTask() []byte {
+	type Task struct {
+		XMLName   			xml.Name 		`xml:"response"`
+		Status				string			`xml:"task_status" json: "task_status"`
+		Description			string			`xml:"task_description"`
+		Type				string			`xml:"task_type"`
+		SolutionTemplate	string			`xml:"solution_template"`
+		CurrentSolution		string			`xml:"current_solution"`
+		ExampleInput		string			`xml:"example_input"`
+		ProgLangList		string			`xml:"prg_lang_list"`
+		HumanLangList		string			`xml:"human_lang_list"`
+		ProgLang			string			`xml:"prg_lang"`
+		HumanLang			string			`xml:"human_lang"`
+	}
+	prg_lang_list, err := json.Marshal([]string{"c", "cpp"})
+	human_lang_list, err := json.Marshal([]string{"en", "cn"})
+
+	task := Task{
+		Status: "open",
+		Description: "Description: task1,en,c",
+		Type: "algo",
+		SolutionTemplate: "",
+		CurrentSolution: "",
+		ExampleInput: "",
+		ProgLangList: string(prg_lang_list),
+		HumanLangList: string(human_lang_list),
+		ProgLang: "c",
+		HumanLang: "en",
+	}
+	xmlResp, err := xml.MarshalIndent(task, " ", "    ")
+	if err != nil {
+		return []byte{}
+	}
+	return xmlResp
+}
--- cui_server/server.go
+++ cui_server/server.go
@@ -5,8 +5,20 @@ import (
 	"net/http"
 )
 
+func handleHttp(w http.ResponseWriter, r *http.Request) {
+	cmd := r.URL.Path[len("/c/"):]
+	if cmd == "_get_task" {
+		w.Header().Set("Content-Type", "text/xml; charset=utf-8")
+		w.Write(getTask())
+	} else {
+		w.Write([]byte("Hello World\n"))
+	}
+}
+
 func main() {
 	// Simple static webserver:
+	http.Handle("/c/", http.HandlerFunc(handleHttp))
 	http.Handle("/cui/", http.StripPrefix("/cui/", http.FileServer(http.Dir("../static_cui/cui/"))))
 	log.Fatal(http.ListenAndServe(":8082", nil))
+	//log.Fatal(http.ListenAndServe(":8088", http.HandlerFunc(handleHttp)))
 }
