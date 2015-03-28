1c783dd94a0adb6a1142bda8e6fa0350fdef5129
maddyonline <madhavjha@gmail.com>
2015-03-27 21:55:09 -0700

    moving to a mux

--- cui_server/server.go
+++ cui_server/server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"io/ioutil"
 )
 
 func handleHttp(w http.ResponseWriter, r *http.Request) {
@@ -15,10 +16,30 @@ func handleHttp(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+const MAIN_HTML = "../static_cui/cui/cui.html"
+
+func htmlResponseHandler(html []byte) http.Handler {
+	fn := func (w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL)
+		w.Write(html)
+	}
+	return http.HandlerFunc(fn)
+}
+
 func main() {
-	// Simple static webserver:
-	http.Handle("/c/", http.HandlerFunc(handleHttp))
-	http.Handle("/cui/", http.StripPrefix("/cui/", http.FileServer(http.Dir("../static_cui/cui/"))))
-	log.Fatal(http.ListenAndServe(":8082", nil))
+	var html []byte
+	var err error
+	if html, err = ioutil.ReadFile(MAIN_HTML); err != nil {
+        log.Fatal(err)
+    }
+
+    mh := htmlResponseHandler(html)
+    fs := http.FileServer(http.Dir("../static_cui/cui/"))
+
+	mux := http.NewServeMux()
+	mux.Handle("/", mh)
+	mux.Handle("/static/", fs)
+	
+	log.Fatal(http.ListenAndServe(":8082", mux))
 	//log.Fatal(http.ListenAndServe(":8088", http.HandlerFunc(handleHttp)))
 }
